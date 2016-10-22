d88251545322186feed9f9b3de1ebc8b524252fc
dhrone <ron@ritchey.org>
2016-10-22 10:51:30 -0400

    Working on music data

--- musicdata.py
+++ musicdata.py
@@ -3,77 +3,77 @@
 import abc
 
 class musicdata:
-    __metaclass__ = abc.ABCMeta
+	__metaclass__ = abc.ABCMeta
 
-    # Children of this class will own interacting with the underlying music service
-    # Must monitor for changes to the players state
-    # Uses a queue (received from caller) to send any updates that occur
+	# Children of this class will own interacting with the underlying music service
+	# Must monitor for changes to the players state
+	# Uses a queue (received from caller) to send any updates that occur
 
-    # Will use a thread to receive updates from service.
-    # Will update musicdata on receipt of new data
-    # Will send a message over the data queue whenever an update is received
-    # Will only send keys that have been updated
+	# Will use a thread to receive updates from service.
+	# Will update musicdata on receipt of new data
+	# Will send a message over the data queue whenever an update is received
+	# Will only send keys that have been updated
 
-    # May use a thread to issue keyalives to the music service if needed
+	# May use a thread to issue keyalives to the music service if needed
 
-    # Future state
-    # Will allow command issuance to music service.
-
-
-    musicdata_init = {
-        'state':u"stop",
-        'musicdatasource':u"",
-        'actPlayer':u"",
-        'artist':u"",
-        'title':u"",
-        'album':u"",
-        'current':0,
-        'remaining':u"",
-        'duration':0,
-        'position':u"",
-        'volume':0,
-        'playlist_display':u"",
-        'playlist_position':0,
-        'playlist_count':0,
-        'bitrate':u"",
-        'type':u""
-    }
-
-    def __init__(self, q):
-        self.musicdata = self.musicdata_init
-        self.musicdata_prev = self.musicdata
-        self.dataqueue = q
+	# Future state
+	# Will allow command issuance to music service.
 
 
-    def sendUpdate(self):
-        # Figure out what has changed and then send just those values across dataqueue
-        md = { }
-        for k, v in musicdata.iteritems():
-            pv = musicdata_prev[k] if k in musicdata_prev else None
-            if pv == v:
-                md[k] = v
+	musicdata_init = {
+		'state':u"stop",
+		'musicdatasource':u"",
+		'actPlayer':u"",
+		'artist':u"",
+		'title':u"",
+		'album':u"",
+		'current':0,
+		'remaining':u"",
+		'duration':0,
+		'position':u"",
+		'volume':0,
+		'playlist_display':u"",
+		'playlist_position':0,
+		'playlist_count':0,
+		'bitrate':u"",
+		'type':u""
+	}
+
+	def __init__(self, q):
+		self.musicdata = self.musicdata_init
+		self.musicdata_prev = self.musicdata
+		self.dataqueue = q
 
 
-        # Send md to queue if anything has changed
-        if len(md) > 0:
-            dataqueue.put(md)
+	def sendUpdate(self):
+		# Figure out what has changed and then send just those values across dataqueue
+		md = { }
+		for k, v in musicdata.iteritems():
+			pv = musicdata_prev[k] if k in musicdata_prev else None
+			if pv == v:
+				md[k] = v
 
-            # Update musicdata_prev
-            musicdata_prev = musicdata
+
+		# Send md to queue if anything has changed
+		if len(md) > 0:
+			dataqueue.put(md)
+
+			# Update musicdata_prev
+			musicdata_prev = musicdata
 
 
-    def clear(self):
-        # revert data back to init state
-        self.musicdata = self.musicdata_init
+	def clear(self):
+		# revert data back to init state
+		self.musicdata = self.musicdata_init
 
 	@abc.abstractmethod
-    def run():
-        # Start thread(s) to monitor music source
-        # Threads must be run as daemons
-        # Future state: start thread to issue commands to music source
-        return
+	def run():
+		# Start thread(s) to monitor music source
+		# Threads must be run as daemons
+		# Future state: start thread to issue commands to music source
+		return
 
-    @abc.abstractmethod
-    def command(cmd):
-        # Send command to music service
-        # Throws NotImplementedError if music service does not support commands
+	@abc.abstractmethod
+	def command(cmd):
+		# Send command to music service
+		# Throws NotImplementedError if music service does not support commands
