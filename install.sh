3e19c90da2b172bdeb9fe1ba9d34db9938e355d3
dhrone <ron@ritchey.org>
2017-10-29 13:51:57 -0400

    Fixed version tag for docker container

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 curl -sSL https://get.docker.com | sh
 sudo systemctl enable docker
 sudo systemctl start docker
-sudo docker pull dhrone/pydpiper:latest
+sudo docker pull dhrone/pydpiper:v0.31-alpha
 sudo cp pydpiper.service /etc/systemd/system
 sudo systemctl enable pydpiper
 sudo systemctl start pydpiper
