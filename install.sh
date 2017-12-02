be7291efb7de576bb3abfc0e32b3500c8f08630b
dhrone <ron@ritchey.org>
2017-12-02 12:32:44 -0500

    Fixing Docker/Raspbian bug by downgrading version

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
-curl -sSL https://get.docker.com | sh
+sudo apt-get install -y --force-yes docker-ce=17.10.0-ce-0raspbian
+sudo usermod -aG docker volumio
 sudo systemctl enable docker
 sudo systemctl start docker
 sudo docker pull dhrone/pydpiper:v0.31-alpha
