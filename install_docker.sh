bb5cc4655e69576b45d78bb9dbccaf6be5b2878f
dhrone <ron@ritchey.org>
2017-12-02 13:56:28 -0500

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -2,12 +2,12 @@ echo 'Installing Docker from the convenience script'
 curl -sSL https://get.docker.com | sh
 
 echo 'Now removing the engine so we can downgrade to one that currently works on Raspberry Pis' 
-sudo apt-get purge docker-ce
-sudo apt-get autoremove
-rm -rf /var/lib/docker # This deletes all images, containers, and volumes
+sudo apt-get purge -y docker-ce
+sudo apt-get autoremove -y 
+sudo rm -rf /var/lib/docker # This deletes all images, containers, and volumes
 
 echo 'Reinstalling an older version'
-sudo apt-get install -y --force-yes docker-ce=17.10.0ce-0raspbian
+sudo apt-get install -y --force-yes docker-ce=17.09.0ce-0raspbian
 
 echo 'Enabling docker'
 sudo usermod -aG docker volumio
