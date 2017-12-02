ea2f331506bd946dcc0f7202e33ecaa1db56a42d
dhrone <ron@ritchey.org>
2017-12-02 12:37:01 -0500

    Limiting this script to installing Docker

--- install.sh
+++ install.sh
@@ -3,6 +3,5 @@ sudo usermod -aG docker volumio
 sudo systemctl enable docker
 sudo systemctl start docker
 sudo docker pull dhrone/pydpiper:v0.31-alpha
-sudo cp pydpiper.service /etc/systemd/system
-sudo systemctl enable pydpiper
-sudo systemctl start pydpiper
+docker run armhf/hello-world
+
