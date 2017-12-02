c27ca2bb26a62ad0eabeadf0e2ef83a512c36853
dhrone <ron@ritchey.org>
2017-12-02 13:35:58 -0500

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -1,7 +1,19 @@
-sudo apt-get update && apt-get install -y --force-yes docker-ce=17.10.0-ce-0raspbian
+echo 'Installing Docker from the convenience script'
+curl -sSL https://get.docker.com | sh
+
+echo 'Now removing the engine so we can downgrade to one that currently works on Raspberry Pis' 
+sudo apt-get purge docker-ce
+sudo apt-get autoremove
+rm -rf /var/lib/docker # This deletes all images, containers, and volumes
+
+echo 'Reinstalling an older version'
+sudo apt-get install -y --force-yes docker-ce=17.10.0ce-0raspbian
+
+echo 'Enabling docker'
 sudo usermod -aG docker volumio
 sudo systemctl enable docker
 sudo systemctl start docker
 sudo docker pull dhrone/pydpiper:v0.31-alpha
-docker run armhf/hello-world
 
+echo 'Testing docker'
+docker run armhf/hello-world
