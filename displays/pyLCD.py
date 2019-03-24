d98125af76799b28f4f2de71639a1d33d43a71dd
dhrone <ron@ritchey.org>
2019-03-24 14:42:20 -0400

    working

--- displays/pyLCD.py
+++ displays/pyLCD.py
@@ -1,4 +1,4 @@
-!/usr/bin/python
+#!/usr/bin/python
 # coding: UTF-8
 
 # Driver for WS0010 OLED driver/controller
@@ -190,4 +190,28 @@ class ws0010(pyLCD):
 		self.lcd.command(self.lcd.c.LCD_SETCGRAMADDR | row)
 
 
-class hd44780(pyLCD):
+#class hd44780(pyLCD):
+
+if __name__ == '__main__':
+    	# Set defaults
+    	# These are for the wiring used by a Raspdac V3
+    	rows = 16
+    	cols = 80
+    	rs = 7
+    	e = 8
+    	d4 = 25
+    	d5 = 24
+    	d6 = 23
+    	d7 = 27
+        pins = [ d4, d5, d6, d7 ]
+
+        interface = interfaceGpio(pin_rs = rs, pin_rw = None, pin_e = e, pins_data = pins)
+        lcd = ws0010(interface)
+
+		print "Winstar OLED Display Test"
+		print "ROWS={0}, COLS={1}, RS={2}, E={3}, Pins={4}".format(rows,cols,rs,e,pins)
+
+		lcd.lcd.clear()
+		lcd.lcd.write_string("pydPiper\nStarting")
+		time.sleep(2)
+		lcd.clear()
