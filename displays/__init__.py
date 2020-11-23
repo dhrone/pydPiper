76db67eecc9ae2206cda9673872be14b4e2222ca
dhrone <ron@ritchey.org>
2020-11-23 08:57:01 -0500

    Support Winstar WEH displays

--- displays/__init__.py
+++ displays/__init__.py
@@ -1,10 +1,11 @@
-__all__ = [ "display", "graphics", "winstar_weg", "ssd1306_i2c", "hd44780", "hd44780_i2c", "luma_i2c", "lcd_curses", "fonts" ]
+__all__ = [ "display", "graphics", "winstar_weg", "winstar_weh", "ssd1306_i2c", "hd44780", "hd44780_i2c", "luma_i2c", "lcd_curses", "fonts" ]
 
 
 import display
 import graphics
 import lcd_curses
 import winstar_weg
+import winstar_weh
 import ssd1306_i2c
 import luma_i2c
 import hd44780
